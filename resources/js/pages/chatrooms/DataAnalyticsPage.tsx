dfd1cb532a9c918467e47517154df744ef7e29f3
franz <franzbugtong0411@gmail.com>
2026-02-25 09:37:40 +0800

    feature : tabs selection for changing plot type, needs refactor

--- resources/js/pages/chatrooms/DataAnalyticsPage.tsx
+++ resources/js/pages/chatrooms/DataAnalyticsPage.tsx
@@ -1,6 +1,6 @@
 import AppLayout from "@/layouts/app-layout";
 import { Head, useForm } from "@inertiajs/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
@@ -9,6 +9,7 @@ import { Upload, FileSpreadsheet } from "lucide-react";
 import Plot from 'react-plotly.js';
 import DescriptiveStatsTable from "@/components/plotly_utils/ui_utils";
 import ReactMarkdown from "react-markdown";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 
 export default function DataAnalyticsPage() {
@@ -17,9 +18,13 @@ export default function DataAnalyticsPage() {
     });
 
     const [analyticsResult, setAnalyticsResult] = useState<any>(null);
-    const [plotlyTraces, setPlotlyTraces] = useState<any[]>([])
     const [descriptiveStats, setDescriptiveStats] = useState<any>({})
     const [AIInterpretation, setAIInterpretation] = useState<string>('')
+    
+    const [plotlyTraces, setPlotlyTraces] = useState<any[]>([])
+    const [plotTypes, setPlotTypes] = useState<string[]>([])
+    const [defaultPlotType, setDefaultPlotType] = useState<string>('')
+    const [selectedPlotType, setSelectedPlotType] = useState<string>('')
 
     const handleAnalyze = () => {
         post(route('data_analytics.analyze'), {
@@ -31,24 +36,65 @@ export default function DataAnalyticsPage() {
                 // Transform the result into Plotly traces
 
                 const traces = result?.analytics_result?.plotly_config?.traces || []
-                setPlotlyTraces(traces)
+                // set the traces after we get the selected plot type from the default
+                // setPlotlyTraces(traces)
 
-                console.log('Plotly Traces from response : ', traces)
+                // console.log('Plotly Traces from response : ', traces)
 
                 const descriptiveStats = result?.analytics_result?.descriptive_statistics || null
                 setDescriptiveStats(descriptiveStats)
 
-                console.log('Descriptive Statistics from response : ', descriptiveStats)
+                // console.log('Descriptive Statistics from response : ', descriptiveStats)
 
                 const ai_report = result?.analytics_result?.ai_interpretation
                 setAIInterpretation(ai_report)
-                console.log('AI Interpretation from response : ', ai_report)
+
+                // console.log('AI Interpretation from response : ', ai_report)
+
+                const plot_types = result?.analytics_result?.plotly_config?.supported_trace_types || []
+                setPlotTypes(plot_types)
+                // console.log('Supported Plot Types from response : ', plot_types)
+
+                const default_plot_type = result?.analytics_result?.plotly_config?.default_plot_type || ''
+                setDefaultPlotType(default_plot_type)
+                console.log('Default Plot Type from response : ', default_plot_type)
+
+                setSelectedPlotType(default_plot_type)
+
+                // after setting the selected plot type we can now filter out the traces
+                setPlotlyTraces(
+                    traces.map((trace: any) => ({
+                        ...trace,
+                        type: default_plot_type
+                    }))
+                )
+
             },
             onError: (errors) => {
                 console.log('Analysis failed:', errors);
             }
         });
     };
+
+    // selected plot mutator
+    useEffect(() => {
+
+        // set the traces  ...
+        setPlotlyTraces((prev) => 
+
+            // for each trace.....
+            prev.map((trace) => ({
+
+                // take the original ......
+                ...trace,
+
+                // ....but only change the trace property the selected type
+                type: selectedPlotType
+            }))
+        )
+
+        // ....trigger only when selectedPLotType is editted
+    }, [selectedPlotType])
 
 
     console.log('Plotly Traces : ', plotlyTraces)
@@ -107,6 +153,23 @@ export default function DataAnalyticsPage() {
                     </Card>
 
                     {/* data visualization using plotly here  */}
+
+                    {/* control for changing plot type */}
+                    {plotTypes.length > 0 && (
+                        <Tabs 
+                            value={selectedPlotType}
+                            onValueChange={setSelectedPlotType}
+                            className='max-w-4xl mx-auto w-full'
+                        >
+                            <TabsList className="grid w-full grid-cols-4">
+                                {plotTypes.map((type => (
+                                    <TabsTrigger key={type} value={type} className='capitalize'>
+                                        {type}
+                                    </TabsTrigger>
+                                )))}
+                            </TabsList>
+                        </Tabs>
+                    )}
 
                     {/* <div className="w-full aspect-square"> */}
                     <div className="max-w-4xl mx-auto aspect-square">
